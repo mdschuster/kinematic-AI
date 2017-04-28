10efcf637bcec0fd25c1a758e7a1ca4da3184ffd
Micah Schuster <schustermd@ornl.gov>
2017-04-28 18:04:14 -0400

    Forgot AI.cs, Added

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour{
+
+	//Baisc AI Script for use with unity object
+	KinematicAICore AICore;
+	Kinematic target;
+	KinematicSteeringOutput KSO;
+
+	//overloaded initializers
+	public void init(KinematicAICore AICore){
+		this.AICore=AICore;
+	}
+
+	public void init(KinematicAICore AICore, Kinematic target){
+		this.AICore = AICore;
+		this.target = target;
+	}
+
+	public void init(){
+		this.AICore=new KinematicWander();
+	}
+
+	//set a new target for the AI
+	public void setTarget(Kinematic target){
+		this.target = target;
+	}
+
+	public Kinematic getTarget(){
+		return this.target;
+	}
+
+	//Change the AI type, must be a kind of KinematicAICore
+	public void setAI(KinematicAICore AICore){
+		if (this.AICore != null) {
+			this.AICore = null;
+		}
+		this.AICore = AICore;
+	}
+	//sets new AI based on string name \\FIXME use a dictionary instead?
+	public void setAI(string AICore){
+		if (this.AICore != null) {
+			this.AICore = null;
+		}
+		if (AICore == "Seek") {
+			this.AICore = new KinematicSeek ();
+		} else if (AICore == "Flee") {
+			this.AICore = new KinematicFlee ();
+		} else {
+			this.AICore = new KinematicWander ();
+		}
+	}
+
+	public KinematicSteeringOutput getSteering(Kinematic self){
+		if (this.target == null) {
+			Debug.Log ("Target is Null, getSteering");
+		}
+		KSO = AICore.getSteering(self,this.target);
+		return this.KSO;
+	}
+
+
+}
